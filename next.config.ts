a40bb95ffcd3839bdb341ccd4a554522ade3e702
adimanab <manab.das@cbnits.com>
2026-02-03 14:50:31 +0530

    auth part initial done

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { dev, isServer }) => {
+    // Enable polling for Docker environments
+    if (dev && !isServer) {
+      config.watchOptions = {
+        poll: 800,       // Check for changes every 800ms
+        aggregateTimeout: 300,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
